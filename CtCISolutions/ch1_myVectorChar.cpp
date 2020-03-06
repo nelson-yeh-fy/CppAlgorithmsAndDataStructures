a68bc550eed93184ce5b9f34bcf3248ba274e5f2
lindon3513 <lindon3513@gmail.com>
2020-03-06 17:28:21 -0500

    Revise my char vector

--- CtCISolutions/ch1_myVectorChar.cpp
+++ CtCISolutions/ch1_myVectorChar.cpp
@@ -5,17 +5,58 @@ using namespace std;
 
 void myVectorChar::doubleSize()
 {
-	char* temp = new char[capacity*2];
-	for (int i = 0; i < capacity; i++) {
-		temp[i] = items[i];
+	m_capacity *= 2;
+	char* temp = new char[m_capacity];
+	for (int i = 0; i < m_capacity /2; i++) {
+		temp[i] = m_items[i];
 	}
-	items = temp;
-	capacity *= 2;
+	m_items = temp;
 	return;
 }
 
-void myVectorChar::halfSize()
+bool myVectorChar::resize(int nsize) {
+	if (nsize < 0 || nsize > INT_MAX)
+		return false;
+
+	if (nsize > m_capacity) {
+		char* temp = new char[nsize];
+		for (int i = 0; i < nsize; i++) {
+			temp[i] = m_items[i];
+		}
+		m_items = temp;
+		m_size = m_capacity = nsize;
+	}
+	else if (nsize < m_size) {
+		// removing those beyond (and destroying them).
+		m_size = nsize;
+	}
+	else if (nsize > m_size) {
+		//  the content is expanded by inserting at the end as many elements as needed to reach a size of n. 
+		m_size = nsize;
+	}
+	return true;
+}
+
+void myVectorChar::reserve(int nsize)
 {
+	if (nsize > m_capacity) {
+		resize(nsize);
+	}
+}
+
+void myVectorChar::shrink_to_fit(int nsize)
+{
+	if (nsize < 0 || nsize > m_capacity || nsize > INT_MAX)
+		return;
+
+	if (nsize < m_capacity) {
+		char* temp = new char[nsize];
+		for (int i = 0; i < nsize; i++) {
+			temp[i] = m_items[i];
+		}
+		m_items = temp;
+		m_size = m_capacity = nsize;
+	}
 }
 
 int myVectorChar::begin()
@@ -25,15 +66,17 @@ int myVectorChar::begin()
 
 int myVectorChar::end()
 {
-	return size-1;
+	return 0;
 }
 
 void myVectorChar::push_back(char c)
 {
-	if (size >= capacity)
+	if (m_size >= m_capacity) {
 		doubleSize();
+		cout << "#doubleSize() is invoked" << endl;
+	}
 
-	items[size] = 'c';
+	m_items[m_size++] = c;
 	return;
 }
 
@@ -41,8 +84,21 @@ void myVectorChar::insert()
 {
 }
 
+void myVectorChar::print()
+{
+	cout << "Capacity:" << m_capacity << " ,Current size:" << m_size << endl;
+	for (int i = 0; i < m_size; i++)
+		cout << m_items[i] << " ";
+	cout << endl;
+}
+
 void demoMyVectorChar1() {
 
 	myVectorChar my;
-	my.push_back(1);
+	my.push_back('a');
+	my.print();
+	my.push_back('b');
+	my.print();
+	my.push_back('c');
+	my.print();
 }
--- CtCISolutions/ch1_myVectorChar.h
+++ CtCISolutions/ch1_myVectorChar.h
@@ -2,25 +2,39 @@
 #define CH1_MYVECTORCHAR_H
 class myVectorChar {
 private:
-	int size;
-	long capacity;
-	char* items;
+	int m_size;
+	int m_max_size;
+	int m_capacity;
+	char* m_items;
 public:
 	myVectorChar() {
-		size = 0;
-		capacity = 2;
-		items = new char[capacity];
+		m_size = 0;
+		m_max_size = INT_MAX;
+		m_capacity = 2;
+		m_items = new char[m_capacity];
 	}
 	~myVectorChar() {
-		if (items)
-			delete[] items;
+		if (m_items)
+			delete[] m_items;
 	}
+
+	// ---
+	// Capacity
+	// ---
+	int size() { return m_size; }
+	int max_size() { return m_max_size; }
+	int capacity() { return m_capacity; }
+	bool resize(int nsize);
+	bool empty() { return m_size == 0; }
+	void reserve(int nsize);
+	void shrink_to_fit(int nsize);
+
 	void doubleSize();
-	void halfSize();
 	int begin();
 	int end();
 	void push_back(char c);
 	void insert();
+	void print();
 };
 
 void demoMyVectorChar1();

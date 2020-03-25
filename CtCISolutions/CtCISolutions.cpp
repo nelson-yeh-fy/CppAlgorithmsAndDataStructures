ba43dac4235a2834b5002f0c1392d37accce6fd6
lindon3513 <lindon3513@gmail.com>
2020-03-25 16:52:42 -0400

    sample

--- CtCISolutions/CtCISolutions.cpp
+++ CtCISolutions/CtCISolutions.cpp
@@ -17,8 +17,31 @@
 //#include "insertionSort_1.h"
 //#include "insertionSort_2.h"
 #include "ch1_myVectorChar.h"
+
 using namespace std;
 
+void ch1_test1() {
+	int ascii_set[128];
+	
+	int img[4][4] = { {0,0,0,0}, {1,1,1,1}, {2,2,2,2}, {3,3,3,3} };
+	std::cout << "." << 'A' % 128 << "." << std::endl;
+
+	std::vector<int> v1(5, 1); //vector with 5 items, all of them are 7,
+	for (int i : v1) {
+		std::cout << i << ",";
+	}
+	std::cout << std::endl;
+
+	std::vector<vector<int>> vv2(5, v1);
+	for (vector<int> v : vv2) {
+		for (int i : v) {
+			std::cout << i << ",";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+}
 int main()
 {
 	//bubbleSort_1_main();
@@ -31,5 +54,7 @@ int main()
 	//demo_minHeap();
 	//demo_maxHeap();
 	//demo_pq();
-	demoMyVectorChar1();
+	//demoMyVectorChar1();
+	ch1_test1();
 }
+
---
+++ CtCISolutions/ch1_strBasics.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void ch1_test0() {
+	std::cout << "d" << std::endl;
+}

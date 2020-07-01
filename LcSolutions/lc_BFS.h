5dec7d84f26563b13316210f9a3d24a668b5f85c
lindon3513 <lindon3513@gmail.com>
2020-07-01 18:02:24 -0400

    lc q98 DFS

--- LcSolutions/lc_BFS.h
+++ LcSolutions/lc_BFS.h
@@ -13,7 +13,7 @@ struct TreeNode {
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-class Solution_q98 {
+class Solution_q98_a {
 public:
     bool isValidBST(TreeNode* root) {
 
@@ -38,6 +38,21 @@ public:
             }
         }
         return true;
+    }
+};
+class Solution_q98_b {
+public:
+    bool isValidBST(TreeNode* node, TreeNode* minNode, TreeNode* maxNode) {
+        if (!node) return true;
+
+        if (minNode && node->val <= minNode->val || maxNode && node->val >= maxNode->val)
+            return false;
+
+        return (isValidBST(node->left, minNode, node) && 
+                isValidBST(node->right, node, maxNode));
+    }
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, nullptr, nullptr);
     }
 };
 //994. Rotting Oranges [Med]

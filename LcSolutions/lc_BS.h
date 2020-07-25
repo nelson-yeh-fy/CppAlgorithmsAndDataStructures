3c3a21810d9b9b22780e42111c2a0302dd9dc65f
lindon3513@gmail.com <lindon3513@gmail.com>
2020-07-25 18:02:36 -0400

    lc q35

--- LcSolutions/lc_BS.h
+++ LcSolutions/lc_BS.h
@@ -84,8 +84,26 @@ public:
         return { seed_l, seed_r };
     }
 };
+//35. Search Insert Position [Easy]
+class Solution_q35 {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                lo = mid + 1;
+            else if (nums[mid] > target)
+                hi = mid - 1;
+        }
+        return lo;
+    }
+};
+
 //278. First Bad Version [Easy] (find lower bound)
-class Solution {
+class Solution_q278 {
 public:
     bool isBadVersion(int version) {
         return true; //fake here, leetcode has real api.
